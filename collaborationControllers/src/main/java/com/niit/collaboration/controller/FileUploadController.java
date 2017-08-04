4768adbec88425881a1b8a00dfaa06499432ac1f
ashikabanu <ashikhabanu37@gmail.com>
2017-08-04 12:22:35 +0530

    this is my second project and i have done some changes

--- collaborationControllers/src/main/java/com/niit/collaboration/controller/FileUploadController.java
+++ collaborationControllers/src/main/java/com/niit/collaboration/controller/FileUploadController.java
@@ -3,20 +3,15 @@ package com.niit.collaboration.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.niit.collaboration.DAO.FileUploadDAO;
@@ -25,6 +20,7 @@ import com.niit.collaboration.model.FileUpload;
 @RestController
 public class FileUploadController
 {
+	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(FileUploadController.class);
 	
 	@Autowired
